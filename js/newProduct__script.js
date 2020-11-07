583db91f0995f589823ba703e39473040097478f
Jhoel Gonzalez <jhoel834@gmail.com>
2020-11-07 11:43:30 -0430

    Adicionando modificacao no codigo do formulario

--- js/newProduct__script.js
+++ js/newProduct__script.js
@@ -12,6 +12,19 @@ addButton.addEventListener("click", (event) => {
         picture: productPicture.src
     }
 
-    localStorage.setItem("product", JSON.stringify(product))
-    /* location.replace(`${location.protocol}//${location.host/index.html}`) */
+    const products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : []
+    products.push(product)
+
+    localStorage.setItem("products", JSON.stringify(products))
+    location.replace(`${location.protocol}//${location.host}/index.html`)
 })
+
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function show() {
+        if (this.files && this.files[0]) {
+            let img = document.querySelector('#productPicture');  // $('img')[0]
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            img.onload = imageIsLoaded;
+        }
+    });
+  });
