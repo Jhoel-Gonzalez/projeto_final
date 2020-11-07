39e65fcd63e2301b62e30937118385bf2451460b
Jhoel Gonzalez <jhoel834@gmail.com>
2020-11-07 11:43:04 -0430

    Adicionando modificacao n codigo do carrinho de compras

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-const newProduct = localStorage.getItem("product")
-const newItemCart = JSON.parse(newProduct)
-const divNewProduct = document.querySelector("#listProduct")
+const productString = localStorage.getItem("products")
+const products = JSON.parse(productString)
+const ulNewProduct = document.querySelector("#listProduct")
 
-let divNova = document.createElement("div"); 
-divNova.innerHTML = 
-`<div>
-    <img class="product__img"
-        src="${newItemCart.picture}"
-        alt="exemplo">
-    <h2 class="product__title">${newItemCart.name}</h2>
-    <p class="product__description">${newItemCart.description}</p>
-    <span>$${newItemCart.price}</span>
-</div>
-<div>
-    <input class="product__input" type="number" min="0" name="pieces" placeholder="1 pcs">
-</div>`
+for (const product of products) {
 
-divNova.classList.add("product__card");
-divNewProduct.appendChild(divNova)
+    let listProduct = document.createElement("li")
+    listProduct.innerHTML = 
+        `<div>
+            <img class="product__img"
+                src="${product.picture}"
+                alt="exemplo">
+            <h2 class="product__title">${product.name}</h2>
+            <p class="product__description">${product.description}</p>
+            <span id="price">R$ ${product.price}</span>
+        </div>
+        <div>
+            <input class="product__input" type="number" min="0" name="pieces" placeholder="1 pcs">
+        </div>`
+
+        listProduct.classList.add("product__card")
+        ulNewProduct.appendChild(listProduct)
+}
+
