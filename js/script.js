7f555856cc6224ecbd25aa73605ab1fb318a28e6
Jhoel Gonzalez <jhoel834@gmail.com>
2020-11-12 16:42:45 -0430

    Adicionando codigo para fazer a suma do total

--- js/script.js
+++ js/script.js
@@ -5,20 +5,56 @@ const ulNewProduct = document.querySelector("#listProduct")
 for (const product of products) {
 
     let listProduct = document.createElement("li")
-    listProduct.innerHTML = 
-        `<div>
+    listProduct.innerHTML =
+        `<div class="image">
             <img class="product__img"
                 src="${product.picture}"
-                alt="exemplo">
+                alt="Imagen do produto">
             <h2 class="product__title">${product.name}</h2>
             <p class="product__description">${product.description}</p>
             <span id="price">R$ ${product.price}</span>
         </div>
         <div>
-            <input class="product__input" type="number" min="0" name="pieces" placeholder="1 pcs">
+            <input id="product__input" class="product__input" type="number" min="0" name="pieces" placeholder="1 pcs" value="1">
         </div>`
 
-        listProduct.classList.add("product__card")
-        ulNewProduct.appendChild(listProduct)
+    listProduct.classList.add("product__card")
+    ulNewProduct.appendChild(listProduct)
 }
 
+const subPrice = document.querySelector("#subTotal")
+const totalPrice = document.querySelector("#total")
+const shippingPrice = document.querySelector("#shipping")
+const taxesPrice = document.querySelector("#taxes")
+
+let subTotal = 0
+let shipping = 0
+let taxes = 0
+let total = 0
+
+for (product of products) {
+    subTotal += Number(product.price)
+    
+    shipping = Math.floor(Math.random() * 100 + 20)
+    taxes =  Math.floor(Math.random() * 30 + 10)
+    
+    subPrice.textContent = `R$ ${subTotal}`
+
+    if (subTotal >= 200) {
+        total = subTotal + taxes
+    } else {
+        total = subTotal + shipping + taxes
+    }
+
+    totalPrice.textContent = `R$ ${total}`
+}
+
+if (subTotal >= 200) {
+    shippingPrice.textContent = 'FREE'
+
+} else {
+
+    shippingPrice.textContent = `R$ ${shipping}`
+}
+
+taxesPrice.textContent = `R$ ${taxes}`
